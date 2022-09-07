e53d60ead23f4ece48b6674b7e3bb1633594aa22
gahyeonn <parka0017@gmail.com>
2022-09-07 21:39:01 +0900

    add : OrderItem 비즈니스 로직 구현

--- src/main/java/com/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/com/jpabook/jpashop/domain/OrderItem.java
@@ -26,4 +26,15 @@ public class OrderItem {
     private int orderPrice; //주문 가격
 
     private int count; //주문 수량
+
+    //==비즈니스 로직==//
+
+    //재고 수량 원복
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }

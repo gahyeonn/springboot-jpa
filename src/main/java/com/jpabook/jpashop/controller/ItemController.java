a2551ea828fe6850d0d897b61a99ae388d3722ff
gahyeonn <parka0017@gmail.com>
2022-09-08 20:55:12 +0900

    add : 상품 등록 기능 구현

---
+++ src/main/java/com/jpabook/jpashop/controller/ItemController.java
@@ -0,0 +1,36 @@
+package com.jpabook.jpashop.controller;
+
+import com.jpabook.jpashop.domain.item.Book;
+import com.jpabook.jpashop.domain.item.Item;
+import com.jpabook.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(BookForm form) {
+        Book book = new Book(); //setter 사용보단 create(생성자 메소드) 만들어서 사용하는게 더 좋음
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/";
+    }
+}
